1ca47e3c644f2725dd8c815d7bd1e0be398b0315
Estevão Henrique <93293869+estevaohp@users.noreply.github.com>
2021-12-17 18:04:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case"1":
+                    ListarSeries();
+                    break;
+
+                    case "2";
+                    InserirSerie();
+                    break;
+
+                    case "3";
+                    AtualizarSerie();
+                    break;
+
+                    case"4";
+                    ExcluirSerie();
+                    break;
+
+                    case"5";
+                    VisualizarSerie();
+                    break;
+
+                    case"C";
+                    Console.Clear();
+                    break;
+
+                    default:
+                    throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario =  ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços!");
+            Console.ReadLine(); 
+        }
+
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar séries");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+            foreach(var serie in lista)
+            {
+                var excluido = serie.retornaExcluido();
+
+                Console.WriteLine("#ID {0}: - {1} {2}", serie.retornaID(), serie.retornaTitulo(), (excluido ? "*Excluido*" : ""));
+            }
+        }
+
+        private static void InserirSerie()
+        {
+            Console.WriteLine("Inserir nova série");
+
+            foreach ( int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o genêro entre as opções acima:");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Tpitulo da Série:");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano da Série:");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição da Série:");
+            string entradaDescricao = Console.ReadLine();
+
+            Series novaSerie = new Series (id: repositorio.ProximoId(),
+            genero: (Genero)entradaGenero,
+            titulo: entradaTitulo,
+            ano: entradaAno,
+            descricao: entradaDescricao);
+
+            repositorio.Insere(novaSerie);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Séries a seu dispor!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1 - Listar  séries");
+            Console.WriteLine("2 - Inserir nova série");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+
+
+            
+        }
+    
+    }
+}
